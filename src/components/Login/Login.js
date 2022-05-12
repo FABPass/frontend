8c40e5ecd2f3acdb655d663a09dc82b49da7fa20
pasic24 <ahmedpasic@hotmail.com>
2022-05-12 23:07:22 +0200

    changed Login component structure

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,31 @@
 import React from 'react';
 import "./Login.css";
 import Logo from "../../resources/fabpass_logo.png"
+import {useRef, useState, useEffect} from 'react';
 
 const Login = () => {
     return (
         <div id="loginform">
             <img src={Logo} alt={"FABPass Logo"} className={"logo"}/>
-            <FormHeader title="Login" />
-            <Form />
-            <OtherMethods />
+            <h2 id="headerTitle">Login</h2>
+            <div>
+                <div className="row">
+                    <label>Email</label>
+                    <input type="text" placeholder="Enter your username"/>
+                </div>
+                <div className="row">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password"/>
+                </div>
+                <div id="button" className="row">
+                    <button>Log in</button>
+                </div>
+            </div>
+            <div id="logRegSwitch">
+                <a href={window.location.protocol + "//" + window.location.host + "/register"}>Don't have an account? Sign up!</a>
+            </div>
         </div>
     );
 };
-
-
-const FormHeader = props => (
-    <h2 id="headerTitle">{props.title}</h2>
-);
-
-
-const Form = props => (
-    <div>
-        <FormInput description="Email" placeholder="Enter your username" type="text" />
-        <FormInput description="Password" placeholder="Enter your password" type="password"/>
-        <FormButton title="Log in"/>
-    </div>
-);
-
-const FormButton = props => (
-    <div id="button" class="row">
-        <button>{props.title}</button>
-    </div>
-);
-
-const FormInput = props => (
-    <div class="row">
-        <label>{props.description}</label>
-        <input type={props.type} placeholder={props.placeholder}/>
-    </div>
-);
-
-const OtherMethods = props => (
-    <div id="logRegSwitch">
-        <a href={window.location.protocol + "//" + window.location.host + "/register"}>Don't have an account? Sign up!</a>
-    </div>
-);
 
 export default Login;
