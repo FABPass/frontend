54d78e617e10e6539ff38c2e7daadc5c91c78513
pasic24 <ahmedpasic@hotmail.com>
2022-05-18 21:58:47 +0200

    created mockup for data item

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Card} from "react-bootstrap";
 
 const Dashboard = () => {
     return (
         <div>
-            TEST
+            <Card
+                bg={"variant".toLowerCase()}
+                key={"variant"}
+                text={'black'}
+                style={{ width: '18rem' }}
+                className="mb-2"
+            >
+                <Card.Header>Header</Card.Header>
+                <Card.Body>
+                    <Card.Title>{"variant"} Card Title </Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
